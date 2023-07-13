ecc1d1e015bbd6470c870ed088e6af3eec329922
ximizhou <11062578+ximizhou@user.noreply.gitee.com>
2023-07-13 13:30:22 +0800

    init

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
+from flask import Flask,render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/index')
 def index():
     return "hello world"
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
 
 if __name__ == '__main__':
     app.run()
